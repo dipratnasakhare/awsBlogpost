5394bf0cd4eeacd310b723e846fda5fb75528d0b
dipratnasakhare <dipratnasakhare11@email.com>
2023-08-21 21:45:36 +0530

    adding blogpost

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import authRoutes from "./Routes/auth.js";
+import userRoutes from "./Routes/users.js";
+import postRoutes from "./Routes/posts.js";
+import {db} from "./db.js"
+import cors from "cors";
+
+
+
+const app = express();
+
+
+app.use(cors())
+app.use(express.json());
+
+
+
+app.get("/api/get", function (req, res) {
+  res.status(200).json({msg:"success"});
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+
+
+
+app.listen(3001, () => {
+  db.connect((err) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('Database connected')
+  })
+});
