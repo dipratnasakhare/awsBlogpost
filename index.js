8a526f8987ed8d91ed62a74886a58c5cbb319240
dipratnasakhare <dipratnasakhare11@email.com>
2023-08-24 10:37:50 +0530

    adding

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-import express from "express";
-import authRoutes from "./Routes/auth.js";
-import userRoutes from "./Routes/users.js";
-import postRoutes from "./Routes/posts.js";
-import cors from "cors";
-import { db } from "./db.js"
+const express = require('express');
+const PostsRouter = require('./routes/posts');
+const AuthRouter = require('./routes/auth');
+const cors = require("cors")
+const {sequelize,connectToDb} = require('./db');
+const body_parser = require('body-parser');
 
 const app = express();
-
 app.use(cors())
 app.use(express.json());
 
+const PORT = 3001;
 
-app.get("/api/get", function (req, res) {
-  res.status(200).json({msg:"welcome to backend"});
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
+app.use(express.json());
+app.use('/api', PostsRouter);
+app.use('/api', AuthRouter);
 
 
-app.listen(3001, () => {
-  db.connect((err) => {
-    if (err) {
-      console.log(err)
-      return
-    }
-    console.log('Database connected')
-  })
-});
+app.get('/api/get/',(request,response)=>{
+    response.status(200).json({message:"Hello World"})
+})
+
+app.listen(PORT , async ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+    await connectToDb();
+})
